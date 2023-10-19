3e8b4d1ac61ca195aaf2d66987cc5424b9c66c06
Jhon Pertuz <pertuzar@hotmail.com>
2023-10-19 17:11:01 -0500

    migraciones y BD sin datos

--- MagicVilla_API/Program.cs
+++ MagicVilla_API/Program.cs
@@ -1,11 +1,25 @@
+using MagicVilla_API.Datos;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+//builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson(); // soporte para que se agregue al servicio
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+// conecta la cadena de conexión con el DbContext,
+// se debe implementar en la clase DbContext el constructor: 
+// public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)  { }
+builder.Services.AddDbContext<ApplicationDbContext>(option => 
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 
 var app = builder.Build();
 
