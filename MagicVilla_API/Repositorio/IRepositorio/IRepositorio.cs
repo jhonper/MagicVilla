0ff74105e6ffab674c6f6b12e08899d2f3066cdc
Jhon Pertuz <pertuzar@hotmail.com>
2023-10-21 07:36:59 -0500

    comenzando mañana 21-10-2023

---
+++ MagicVilla_API/Repositorio/IRepositorio/IRepositorio.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace MagicVilla_API.Repositorio.IRepositorio
+{
+    // Interfaz de repositorio genérico
+    public interface IRepositorio<T> where T : class
+    {
+        Task Crear(T entidad);
+
+        // el ? es para que en caso de que no envíe filtro, sólo devolverá la lista
+        Task<List<T>> ObtenerTodos(Expression<Func<T, bool>>? filtro = null );
+        Task<T> Obtener(Expression<Func<T, bool>>? filtro = null, bool tracked = true);
+        Task Remover(T entidad);
+        Task Grabar(); //método que se encargará del SaveChaged()
+    }
+}
